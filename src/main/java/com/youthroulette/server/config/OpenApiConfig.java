ff981e2951365bce061368aa5fb23adb19930653
Nayeon07 <kimny5726@gmail.com>
2026-07-03 18:45:20 +0900

    feat: swagger setting2

---
+++ src/main/java/com/youthroulette/server/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.youthroulette.server.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "JWT";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Youth Spin API")
+                        .description("버킷리스트 룰렛 서비스 API 문서")
+                        .version("v1.0"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
